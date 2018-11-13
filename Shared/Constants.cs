d5da7a0bc8ea6de4662430f1f93e2e82a7424999
Michal Steyn <michal.steyn@icm.aero>
2018-11-13 11:24:43 -0800

    Commit missing code

---
+++ Shared/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public static class Constants
+    {
+        public const string ServiceQueue = "AppDTestService";
+    }
+}
---
+++ Shared/GetValuesRequestMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Shared
+{
+    public class GetValuesRequestMessage
+    {
+        public int Index { get; set; }
+    }
+
+    public class GetValuesResponseMessage
+    {
+        public string Message { get; set; }
+    }
+}
